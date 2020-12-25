75f502fe02eccb63cdce20199f6fa69dcacb4158
Tobias Schwarz <dastiii@schwarz.id>
2020-12-25 23:09:46 +0100

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,40 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   purge: [
-    './src/components/**/*.vue',
-    './src/views/**/*.vue',
-    './public/index.html',
+    "./src/components/**/*.vue",
+    "./src/views/**/*.vue",
+    "./public/index.html"
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter", ...defaultTheme.fontFamily.sans]
       },
 
       colors: {
-        brand: '#e2b007',
+        brand: "#e2b007",
       },
       rotate: {
-        10: '10deg',
-        '-10': '-10deg',
-      },
-    },
+        "10": "10deg",
+        "-10": "-10deg"
+      }
+    }
   },
   variants: [
-    'responsive',
-    'group-hover',
-    'focus-within',
-    'first',
-    'last',
-    'odd',
-    'even',
-    'hover',
-    'focus',
-    'active',
-    'visited',
-    'disabled',
+    "responsive",
+    "group-hover",
+    "focus-within",
+    "first",
+    "last",
+    "odd",
+    "even",
+    "hover",
+    "focus",
+    "active",
+    "visited",
+    "disabled"
   ],
-  plugins: [],
+  plugins: []
 };
