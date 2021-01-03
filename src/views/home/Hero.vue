1bbbff266bf10f55ac1b6a26e9873ff4215a0eeb
Tobias Schwarz <dastiii@schwarz.id>
2021-01-03 20:34:26 +0100

    wip

--- src/views/home/Hero.vue
+++ src/views/home/Hero.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="relative bg-white overflow-hidden border-b-8 border-brand">
+  <div class="relative bg-gray-800 overflow-hidden border-b-8 border-brand">
     <div class="max-w-7xl mx-auto">
-      <div class="relative z-10 bg-white lg:max-w-2xl lg:w-full">
-        <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
+      <div class="relative z-10 bg-gray-800 lg:max-w-2xl lg:w-full">
+        <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-gray-800 transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
           <polygon points="50,0 100,0 50,100 0,100" />
         </svg>
 
         <main class="mx-auto max-w-7xl px-4 lg:pl-0 lg:pr-24">
           <div class="sm:text-center lg:text-left py-8">
             <h2 class="text-base font-semibold tracking-wide uppercase text-brand">Big G Goods - General Store Merchants</h2>
-            <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-100 sm:text-4xl">
               Dein Gemischtwaren- und Sonderpostenhändler
             </p>
-            <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            <p class="mt-4 max-w-2xl text-xl text-gray-400 lg:mx-auto">
               Wir haben auch, was andere wegwerfen würden. Stöbere einfach in unserem stets aktuellen Katalog an Produkten, die wir dir anbieten können.
             </p>
           </div>
