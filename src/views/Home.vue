81e35ec7d3692c66acced7dc1521cbd555bf47b7
Tobias Schwarz <dastiii@schwarz.id>
2021-01-09 16:13:11 +0100

    wip

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -34,20 +34,22 @@
         </div>
       </div>
     </div>
-    <div class="w-1/3 px-8 space-y-8">
-      <div class="px-8 space-y-4">
-        <h3 class="text-sm text-gray-700 uppercase">Suche</h3>
-        <div class="mt-1 relative rounded-md shadow-sm w-full">
-          <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-            <svg class="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+    <div class="w-1/3">
+      <div class="sticky top-4 space-y-8">
+        <div class="px-8 space-y-4">
+          <h3 class="text-sm text-gray-700 uppercase">Suche</h3>
+          <div class="mt-1 relative rounded-md shadow-sm w-full">
+            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+              <svg class="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+            </div>
+            <input v-model="search" type="text" class="outline-none focus:ring focus:ring-red-500 pr-3 py-2 focus:border-red-500 block w-full pl-10 sm:text-sm border-gray-300 rounded-md" placeholder="z.B. Golderz">
           </div>
-          <input v-model="search" type="text" class="outline-none focus:ring focus:ring-red-500 pr-3 py-2 focus:border-red-500 block w-full pl-10 sm:text-sm border-gray-300 rounded-md" placeholder="z.B. Golderz">
         </div>
-      </div>
-      <div class="px-8 space-y-3">
-        <h3 class="text-sm text-gray-700 uppercase">Filter</h3>
-        <div>
-          <tag v-for="tag in tags" :key="tag" :name="tag" @added="addTag" @removed="removeTag"></tag>
+        <div class="px-8 space-y-3">
+          <h3 class="text-sm text-gray-700 uppercase">Filter</h3>
+          <div>
+            <tag v-for="tag in tags" :key="tag" :name="tag" @added="addTag" @removed="removeTag"></tag>
+          </div>
         </div>
       </div>
     </div>
